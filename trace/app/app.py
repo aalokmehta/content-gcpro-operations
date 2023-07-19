f8d106056e9bff7cadbc80ba3e51c4aaebe18fa5
Aalok Mehta <amehta@splunk.com>
2023-07-18 21:06:03 -0400

    Update example for OpenTelemetry

--- trace/app/app.py
+++ trace/app/app.py
@@ -11,74 +11,80 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """
-A sample app demonstrating Stackdriver Trace
+A sample app demonstrating CloudTraceSpanExporter
 """
-import argparse
+
+import os
 import random
 import time
 
+import flask
+
 # [START trace_demo_imports]
-from flask import Flask
-from opencensus.ext.flask.flask_middleware import FlaskMiddleware
-from opencensus.ext.stackdriver.trace_exporter import StackdriverExporter
-from opencensus.trace import execution_context
-from opencensus.trace.propagation import google_cloud_format
-from opencensus.trace.samplers import AlwaysOnSampler
+from opentelemetry import trace
+from opentelemetry.exporter.cloud_trace import CloudTraceSpanExporter
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.propagate import set_global_textmap
+from opentelemetry.propagators.cloud_trace_propagator import CloudTraceFormatPropagator
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
 # [END trace_demo_imports]
 import requests
 
 
-app = Flask(__name__)
+# [START trace_demo_create_exporter]
+def configure_exporter(exporter):
+    """Configures OpenTelemetry context propagation to use Cloud Trace context
+
+    Args:
+        exporter: exporter instance to be configured in the OpenTelemetry tracer provider
+    """
+    set_global_textmap(CloudTraceFormatPropagator())
+    tracer_provider = TracerProvider()
+    tracer_provider.add_span_processor(BatchSpanProcessor(exporter))
+    trace.set_tracer_provider(tracer_provider)
+
+
+configure_exporter(CloudTraceSpanExporter())
+tracer = trace.get_tracer(__name__)
+# [END trace_demo_create_exporter]
+
 
 # [START trace_demo_middleware]
-propagator = google_cloud_format.GoogleCloudFormatPropagator()
-
-
-def createMiddleWare(exporter):
-    # Configure a flask middleware that listens for each request and applies automatic tracing.
-    # This needs to be set up before the application starts.
-    middleware = FlaskMiddleware(
-        app,
-        exporter=exporter,
-        propagator=propagator,
-        sampler=AlwaysOnSampler())
-    return middleware
+app = flask.Flask(__name__)
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
 # [END trace_demo_middleware]
 
 
-@app.route('/')
-def template_test():
+@app.route("/")
+def template_test() -> str or (str, int):
     # Sleep for a random time to imitate a random processing time
     time.sleep(random.uniform(0, 0.5))
-    # Keyword that gets passed in will be concatenated to the final output string.
-    output_string = app.config['keyword']
-    # If there is no endpoint, return the output string.
-    url = app.config['endpoint']
-    if url == "":
-        return output_string, 200
-    # Endpoint is the next service to send string to.
-    data = {'body': output_string}
+
+    # If there is an endpoint, send keyword to next service.
+    # Return received input with the keyword
+    keyword = os.getenv("KEYWORD")
+    endpoint = os.getenv("ENDPOINT")
     # [START trace_context_header]
-    trace_context_header = propagator.to_header(execution_context.get_opencensus_tracer().span_context)
-    response = requests.get(
-        url,
-        params=data,
-        headers={
-          'X-Cloud-Trace-Context' : trace_context_header}
-    )
-    # [END trace_context_header]
-    return response.text + app.config['keyword']
+    if endpoint is not None and endpoint != "":
+        data = {"body": keyword}
+        response = requests.get(
+            endpoint,
+            params=data,
+        )
+        return keyword + "\n" + response.text
+    else:
+        return keyword, 200
+
+
+# [END trace_context_header]
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--keyword",  default="", help="name of the service.")
-    parser.add_argument("--endpoint", default="", help="endpoint to dispatch appended string, simply respond if not set")
-    args = parser.parse_args()
-    app.config['keyword'] = args.keyword
-    app.config['endpoint'] = args.endpoint
-    # [START trace_demo_create_exporter]
-    createMiddleWare(StackdriverExporter())
-    # [END trace_demo_create_exporter]
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    port = os.getenv("PORT")
+    app.run(debug=True, host="0.0.0.0", port=port)
--- trace/setup.sh
+++ trace/setup.sh
@@ -1,53 +1,28 @@
 #!/bin/bash
-################## Set up service a ###########################
 
-echo "Creating service a"
-kubectl apply -f app/acg-service-a.yaml
+set -o errexit  # exit on error
+SCRIPT_DIR=$(realpath $(dirname "$0"))
+pushd $SCRIPT_DIR > /dev/null
 
-################## Set up service b ###########################
-echo "Fetching the external IP of service a"
-endpoint=""
+echo ################## Set up cloud trace demo application ###########################
+kubectl apply -f app/cloud-trace-acg.yaml
+
+echo ""
+echo -n "Wait for load balancer initialization complete."
 for run in {1..20}
 do
-  echo "Attempt #${run} to fetch the external IP of service a..."
   sleep 5
   endpoint=`kubectl get svc cloud-trace-acg-a -ojsonpath='{.status.loadBalancer.ingress[0].ip}'`
   if [[ "$endpoint" != "" ]]; then
     break
   fi
+  echo -n "."
 done
 
-if [[ "$endpoint" == "" ]]; then
-  echo "Unable to get external IP for service cloud-trace-acg-a"
-  exit 1
+echo ""
+if [ -n "$endpoint" ]; then
+  echo "Completed. You can access the demo at http://${endpoint}/"
+else
+  echo "There is a problem with the setup. Cannot determine the endpoint."
 fi
-
-echo "Passing external IP for the first service ${endpoint} to the second service template"
-sed "s/{{ endpoint }}/${endpoint}/g" app/acg-service-b.yaml.template > app/acg-service-b.yaml
-kubectl apply -f app/acg-service-b.yaml
-rm app/acg-service-b.yaml
-
-################## Set up service c ###########################
-echo "Fetching the external IP of service b"
-endpoint=""
-for run in {1..20}
-do
-  echo "Attempt #${run} to fetch the external IP of service b..."
-  sleep 5
-  endpoint=`kubectl get svc cloud-trace-acg-b -ojsonpath='{.status.loadBalancer.ingress[0].ip}'`
-  if [[ "$endpoint" != "" ]]; then
-    break
-  fi
-done
-
-if [[ "$endpoint" == "" ]]; then
-  echo "Unable to get external IP for service cloud-trace-acg-a"
-  exit 1
-fi
-
-echo "Passing external IP for the service b ${endpoint} to the service c"
-sed "s/{{ endpoint }}/${endpoint}/g" app/acg-service-c.yaml.template > app/acg-service-c.yaml
-kubectl apply -f app/acg-service-c.yaml
-rm app/acg-service-c.yaml
-
-echo "Successfully deployed all services"
+popd
